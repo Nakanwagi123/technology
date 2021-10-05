091116400320eb6a7863c76aca24e5548ab0cf23
mayanja andrew <nakanwagitreasure@gmail.com>
2021-10-05 19:35:24 +0300

    chaneg

--- src/pages/fixed/Nav.js
+++ src/pages/fixed/Nav.js
@@ -1,1 +1,29 @@
-hello
+import React from "react";
+import "./navbar.css";
+import {Link} from "react-router-dom";
+
+function Nav(){
+
+    
+    return(
+    <>
+    <header>
+        <h1 class="logo">Logo</h1>
+        <input type="checkbox" id="nav-toggle" class="nav-toggle"/>
+        <nav>
+            <ul>
+            <li><a href="#">Profile</a></li>
+            <li><Link to="/" class="link">Menuu</Link></li>
+            <li><a href="#">RideHistory</a></li>
+            <li><a href="#">Support</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Signup to drive</a></li>
+            </ul>
+        </nav>
+        <label for="nav-toggle" class="nav-toggle-label"><span></span></label>
+    </header>
+    </>
+        );
+}
+
+export default Nav;
