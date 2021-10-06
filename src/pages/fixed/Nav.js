bb1bc8a3e613feba2c8a12d8325cee6aec2de514
mayanja andrew <nakanwagitreasure@gmail.com>
2021-10-06 12:09:50 +0300

    navi

--- src/pages/fixed/Nav.js
+++ src/pages/fixed/Nav.js
@@ -8,15 +8,18 @@ function Nav(){
     return(
     <>
     <header>
-        <h1 class="logo">Logo</h1>
-        <input type="checkbox" id="nav-toggle" class="nav-toggle"/>
+    <div class="closure">
+    <div class="logos"><h1 class="logo">Logo</h1></div>
+    <div class="notification"> <p>not</p></div>
+    </div>
+          <input type="checkbox" id="nav-toggle" class="nav-toggle"/>
         <nav>
             <ul>
             <li><Link to="/account" class="link">Account</Link></li>
             <li><Link to="/" class="link">Menu</Link></li>
             <li><a href="#">RideHistory</a></li>
             <li><a href="#">Support</a></li>
-            <li><a href="#">About</a></li>
+            <li><a href="#">Settings</a></li>
             <li><a href="#">Signup to drive</a></li>
             </ul>
         </nav>
