5d664d771bf1e809cce9ecbe6eb7af78a1da0def
mayanja andrew <nakanwagitreasure@gmail.com>
2021-10-12 12:53:51 +0300

    first commit

--- src/pages/others/Home.js
+++ src/pages/others/Home.js
@@ -5,6 +5,18 @@ import Notf from "../hooks/Notf";
 
 
 class Home extends React.Component{
+    /**carousel=()=>{
+        const track=document.querySelector('.carousel__track');
+        const slides=Array.from(track.children);
+        const nextbutton=document.querySelector('carousel__button--right');
+        const prevbutton=document.querySelector('carousel__button--left');
+        const dotsNav=document.querySelector('carousel__nav');
+        const dots=Array.from(dotsNav.children);
+        const slideWidth=slides[0].getBoundingClientRect().width;
+        slides[0].style.left=0;
+        slides[1].style.left=slideWidth+'px';
+        console.log(slides);
+    }**/
    
     render(){
         return(
@@ -15,6 +27,24 @@ class Home extends React.Component{
            <div class="placeorder"><p>Lets get you there!</p><Link to="/order"><button><h4>Order a Ride</h4></button></Link></div>
            <div class="slider"></div>
            <div class="welcome"><p>WELCOME TO RIDEMYWAY!</p></div>
+
+           <div class="carousel">
+           <button class="carousel__button carousel__button--left">n</button>
+           <div class="carousel__track-container">
+             <ul class="carousel__track">
+               <li class="carousel__slide"><div class="carousel__image"></div></li>
+               <li class="carousel__slide"><div class="carousel__image"></div></li>
+               <li class="carousel__slide"><div class="carousel__image"></div></li>
+             </ul>
+           </div>
+           <button class="carousel__button carousel__button--right">p</button>
+           <div class="carousel__nav">
+               <button class="carousel__indicator current-slide"></button>
+               <button class="carousel__indicator"></button>
+               <button class="carousel__indicator"></button>
+           </div>
+           </div>
+         
            </div>
        </div>
    </section>
